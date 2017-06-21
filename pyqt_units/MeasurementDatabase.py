ad1deb8e5463bf0b7cbbb13a988af82ca57a9ff4
Neil Butcher <nbutcher@fmail.co.uk>
2017-06-21 22:19:15 +0100

    split the database into a root and a temp parts so the changes to current unit aren't committed

--- pyqt_units/MeasurementDatabase.py
+++ pyqt_units/MeasurementDatabase.py
@@ -3,5 +3,11 @@ Created on 12 Aug 2014
 
 @author: neil.butcher
 '''
+import os
+from shutil import copyfile
+root_filename =  os.path.dirname(__file__) + '/Measurements_root.db'
 
-filename = 'Measurements.db'
+filename = 'Measurements_temp.db'
+
+if not os._exists(filename):
+    copyfile(root_filename,filename)
