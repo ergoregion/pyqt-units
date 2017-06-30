2a238244153a72221b94ed8d10405ba354dbe937
Neil Butcher <nbutcher@fmail.co.uk>
2017-06-30 12:56:26 +0100

    v 1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 setup(
     name='pyqt-units',
     packages=find_packages(exclude=[]),
-    version='1.1',
+    version='1.2',
     description='System of units for PyQt',
     author='Neil Butcher',
     url='https://github.com/ergoregion/pyqt-units.git',
@@ -13,7 +13,7 @@ setup(
     install_requires=[],
     python_requires='>=3',
     package_data={
-        'pyqt_units': [os.path.join('pyqt_units','Measurements','measurements_root.db')],
+        'pyqt_units': [os.path.join('Measurements','measurements_root.db')],
     },
     classifiers=[],
 )
