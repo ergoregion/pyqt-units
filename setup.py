13cac6d8bc94252435f84d21859ee4f280efa6e6
Neil Butcher <nbutcher@fmail.co.uk>
2017-07-13 10:43:03 +0100

    v1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 setup(
     name='pyqt-units',
     packages=find_packages(exclude=[]),
-    version='1.6',
+    version='1.7',
     description='System of units for PyQt',
     author='Neil Butcher',
     url='https://github.com/ergoregion/pyqt-units.git',
