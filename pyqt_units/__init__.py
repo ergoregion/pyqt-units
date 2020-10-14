e023d5279fcc26a1b8f7dc0b9e5bf03400532844
ergoregion <neil.butcher@gmail.com>
2020-10-14 14:54:05 +0100

    v3.3

--- pyqt_units/__init__.py
+++ pyqt_units/__init__.py
@@ -54,4 +54,4 @@ def changedSignal():
     return _setter.changed
 
 def measurements_list():
-    return('Length','Angle','Pressure','Temperature','Volume', 'Mass', 'Proportion', 'Density', 'Force', 'Power', 'Time', 'AngularSpeed', 'Torque', 'None', 'Speed', 'Inertia', 'Area', 'Current', 'Voltage', 'Magnetic Flux Linkage', 'Flux Density', 'Inductance', 'Resistance')
+    return('Length','Angle','Pressure','Temperature','Volume', 'Mass', 'Proportion', 'Density', 'Force', 'Power', 'Time', 'AngularSpeed', 'Torque', 'None', 'Speed', 'Inertia', 'Area', 'Current', 'Voltage', 'Magnetic Flux Linkage', 'Flux Density', 'Inductance', 'Resistance', 'Current Density', 'Torque Per Unit Current', 'Voltage Per Unit AngularSpeed')
